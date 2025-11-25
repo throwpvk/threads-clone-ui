fcf66e0584debe97983c8c6c94c5abfffccdbd6b
Phan Van Khai <phanvankhaiofficial@gmail.com>
2025-11-25 23:15:42 +0900

    fix create card header hover bg

--- src/components/layouts/components/CreateThreadHeader.jsx
+++ src/components/layouts/components/CreateThreadHeader.jsx
@@ -8,17 +8,17 @@ export const CreateThreadHeader = ({ onClose, isModal = true }) => {
     <CardHeader className="border-b border-border h-14 px-6! flex items-center justify-between">
       <div className="flex items-center justify-between flex-1">
         <button
-          className="h-auto px-0 hover:bg-transparent text-base font-normal"
+          className="h-auto px-0 hover:bg-transparent cursor-pointer text-base font-normal"
           onClick={onClose}
         >
           {isModal ? "Cancel" : <XIcon className="size-5" />}
         </button>
         <CardTitle className="text-base font-semibold">New thread</CardTitle>
         <CardAction className="flex items-center gap-2">
-          <button className="h-8 w-8 rounded-full hover:bg-accent flex items-center justify-end">
+          <button className="h-8 w-8 rounded-full hover:bg-transparent cursor-pointer flex items-center justify-end">
             <DraftIcon />
           </button>
-          <button className="h-8 w-8 rounded-full hover:bg-accent flex items-center justify-end">
+          <button className="h-8 w-8 rounded-full hover:bg-transparent cursor-pointer flex items-center justify-end">
             <MoreIcon />
           </button>
         </CardAction>
