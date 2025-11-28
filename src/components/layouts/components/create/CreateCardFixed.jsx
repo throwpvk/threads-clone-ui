12468458196afb7a8caf182f04b2acc3fd4e5aaf
Phan Van Khai <215381564+throwpvk@users.noreply.github.com>
2025-11-28 11:41:23 +0900

    add modal-lock-scroll

--- src/components/layouts/components/create/CreateCardFixed.jsx
+++ src/components/layouts/components/create/CreateCardFixed.jsx
@@ -40,11 +40,11 @@ export const CreateCardFixed = ({
   useEffect(() => {
     if (isModal || isMobile) {
       const scrollY = window.scrollY;
-      document.body.classList.add("lock-scroll");
+      document.body.classList.add("modal-lock-scroll");
       document.body.style.top = `-${scrollY}px`;
 
       return () => {
-        document.body.classList.remove("lock-scroll");
+        document.body.classList.remove("modal-lock-scroll");
         document.body.style.top = "";
         window.scrollTo(0, scrollY);
       };
